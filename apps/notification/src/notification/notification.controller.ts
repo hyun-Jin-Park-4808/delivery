2f15daefb6283de20cf74954f1aff57bceeece25
박현진 <hyunjin@petch.com>
2026-02-10 07:50:30 +0900

    feat: RpcInterceptor notification에 적용

--- apps/notification/src/notification/notification.controller.ts
+++ apps/notification/src/notification/notification.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { SendPaymentNotificationDto } from './dto/send-payment-notifiction.dto';
 import { NotificationService } from './notification.service';
+import { RpcInterceptor } from '@app/common';
 
 @Controller()
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
 
   @MessagePattern({ cmd: 'send_payment_notification' })
+  @UseInterceptors(RpcInterceptor)
   @UsePipes(ValidationPipe)
   async sendPaymentNotification(
     @Payload() payload: SendPaymentNotificationDto,
